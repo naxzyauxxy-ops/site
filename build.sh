94bc9c1a59326cdfaa1de1b7c2b840f2d8eb8739
naxzyauxxy-ops <naxzyauxxy@gmail.com>
2026-03-12 07:05:49 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 set -e
-
-# Install npm deps
 npm install
-
-# Install bun
 curl -fsSL https://bun.sh/install | bash
-export PATH="$HOME/.bun/bin:$PATH"
-
-# Clone BlooketFlooder if not present
-if [ ! -d "flooder" ]; then
-  git clone https://github.com/VillainsRule/BlooketFlooder flooder
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+if [ ! -d "BlooketFlooder" ]; then
+  git clone https://github.com/VillainsRule/BlooketFlooder
 fi
-
-# Install flooder deps
-cd flooder && bun install && cd ..
-
-echo "Build complete!"
+cd BlooketFlooder && bun i && cd ..
+echo "Build done"
